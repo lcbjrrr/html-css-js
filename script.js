1db3bf5d009ef73791afcbe41e9be3c27eff7aa8
lcbjrrr <lcbj@cesar.school>
2020-07-14 19:03:24 -0300

    v2

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function alo(){
-    alert('alo');
-    console.log('alo')
+function popula_tabela(){
+    var palestras = [{data:"1/2/20",host:"Luiz",theme:"Javascript"},{data:"1/3/20",host:"Joao",theme:"HTML"},{data:"1/4/20",host:"Maria",theme:"CSS"}  ]
+    var tab = document.getElementById("tabela");
+    console.log(palestras.length)
+    for(var i=0;i<palestras.length;i++){
+        pal = palestras[i]
+        console.log(palestras[i])
+        var tr = tab.insertRow(i)
+        console.log(tr)
+        var td = tr.insertCell(0)
+        td.innerHTML=pal["data"]
+        var td = tr.insertCell(1)
+        td.innerHTML=pal["host"]
+        var td = tr.insertCell(2)
+        td.innerHTML=pal["theme"]
+        
+        
+    }
+
 }
+
